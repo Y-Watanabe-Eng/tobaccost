92fd16054cd461a37498c42fd232a04f985a98ee
Y.W <130629956+Y-Watanabe-Eng@users.noreply.github.com>
2023-11-11 12:34:13 +0900

    Left.jsx の更新

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -9,8 +9,8 @@ export const Left = (props) => {
   
         <div className="grid grid-cols-2 pt-5">
             <div className="inline-block">
-            <div className="h-12 text-left">              
-                <h3 className="text-left">情報入力</h3>
+            <div className="h-12 inline-block text-left">              
+                <h3>テスト情報入力</h3>
             </div>
             <div className="h-12 text-left">
                 <p className="h-12">1箱の値段：</p>
